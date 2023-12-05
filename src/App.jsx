a354540714816d21e0f29f437c2823462816d117
amitg01 <amitg.ghy@gmail.com>
2023-12-05 23:27:38 +0530

    configured tailwindcss and sign in and sign up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<h1>Home</h1>} />
+      <Route path="signup" element={<SignUp />} />
+      <Route path="signin" element={<SignIn />} />
       <Route path="about" element={<h1>About</h1>} />
-      <Route path="*" element={<h1>Not Found</h1>} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
